72f6a82d3e018ce464158ad7caed6c99bf53d9ff
JonatanWang <Jonatan.Wang@gmail.com>
2020-12-05 19:45:52 +0100

    Add solution part 2: get the seat id

--- src/Day05/BinaryBoarding.java
+++ src/Day05/BinaryBoarding.java
@@ -3,6 +3,7 @@ package Day05;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -74,13 +75,37 @@ public class BinaryBoarding {
         var result = Integer.MIN_VALUE;
         result = input.stream().mapToInt(charArray -> {
             var seatId = getSeatId(getRowNumber(charArray), getColumnNumber(charArray));
-            System.out.print(" " + seatId);
+            //System.out.print(" " + seatId);
             return seatId;}).summaryStatistics().getMax();
+
+        return result;
+    }
+
+    private static List<Integer> getSeatIds() {
+        var result = new ArrayList<Integer>();
+        result = (ArrayList<Integer>) input.stream().map(charArray -> {
+            var seatId = getSeatId(getRowNumber(charArray), getColumnNumber(charArray));
+            return seatId;}).sorted().collect(Collectors.toList());
+
+        //result.stream().forEach(System.out::println);
+
+        return result;
+    }
+
+    private static int getMySeatId() {
+        var idList = getSeatIds();
+        var result = Integer.MAX_VALUE;
+        for (var i = 0; i < idList.size() - 1; i ++) {
+            if (idList.get(i) +1 < idList.get(i + 1)) {
+                result = idList.get(i) + 1;
+            }
+        }
 
         return result;
     }
 
     public static void main(String[] args) {
         System.out.println("Highest Seat ID: " + getHighestSeatId());
+        System.out.println(getMySeatId());
     }
 }
